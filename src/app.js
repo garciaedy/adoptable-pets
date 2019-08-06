1803f132ba9eafd2486da2718e5e1871edf78777
Edy Garcia <edygarcia@edys-imac.attlocal.net>
2019-08-05 22:58:39 -0400

    react dom

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
 const Pet = ({ name, animal, breed }) => {
     return React.createElement("div", {}, [
         React.createElement('h1', {}, name),
@@ -22,6 +25,7 @@ const App = () => {
                 name: 'Snar',
                 animal: 'dog',
                 breed: 'Doberman'
+
             }),
 
             React.createElement(Pet,
@@ -34,4 +38,4 @@ const App = () => {
     );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
