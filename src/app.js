5a03e7d688e08b9c0a6937bb2da1be394cf51f69
Edy Garcia <edygarcia@edys-imac.attlocal.net>
2019-08-05 21:28:12 -0400

    passed props

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-const Pet = () =>{
+const Pet = (props) =>{
     return React.createElement("div", {}, [
-        React.createElement('h1',{}, "hommie"),
-        React.createElement('h2',{}, "Snar"),
-        React.createElement('h2',{}, "perro")
+        React.createElement('h1',{}, props.name),
+        React.createElement('h2',{}, props.animal),
+        React.createElement('h2',{}, props.breed)
     ])
 };
 
